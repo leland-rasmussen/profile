31e5685c8d16d8a7fdedd11819c0276af651ae81
Leland Rasmussen <leland.rasmussen@podium.com>
2020-01-16 19:08:58 -0700

    stuff

--- scripts.js
+++ scripts.js
@@ -11,4 +11,34 @@ function setActiveTab(tab) {
 
     // set the new tab to have the active class
     tab.classList.add('active-link')
+
+    //scroll to element
+    const x = document.querySelector("." + tab.id)
+    smoothScroll(x, 1)
+}
+
+function smoothScroll(target, duration) {
+    const targetPosition = target.getBoundingClientRect().top
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    console.log(targetPosition)
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        console.log(run)
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t / + d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
 }
